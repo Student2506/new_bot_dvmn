6f1f9e862eebd2a8946683d98d88f7dcff0149d1
Захаров Павел <pzakharov83@gmail.com>
2022-01-14 09:37:51 +0300

    added logging

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import logging
 import os
 import time
+
 import requests
 import telegram
-
 
 BASE_URL = 'https://dvmn.org/api/long_polling/'
 CBOT_DVMN_KEY = os.environ['DVMN_KEY']
@@ -10,7 +11,15 @@ CBOT_BOT_TOKEN = os.environ['BOT_TOKEN']
 CBOT_CHAT_ID = os.environ['CHAT_ID']
 TIMEOUT = 90
 
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
+
+
 def main():
+    logging.debug('Бот стартовал')
     delay = 10
     timestamp = time.time()
     headers = {'Authorization': f'Token {CBOT_DVMN_KEY}'}
@@ -18,7 +27,11 @@ def main():
     while True:
         params = {"timestamp": timestamp}
         try:
-            response = requests.get(BASE_URL, headers=headers, params=params, timeout=(TIMEOUT+delay))
+            response = requests.get(
+                BASE_URL, headers=headers, params=params, timeout=(
+                    TIMEOUT+delay
+                )
+            )
             response.raise_for_status()
             homeworks = response.json()
             if 'timeout' in homeworks:
@@ -28,27 +41,28 @@ def main():
                 results = homeworks.get('new_attempts', [])
                 for result in results:
                     result_title = result.get('lesson_title')
-                    result_url = result.get('lesson_url')    
+                    result_url = result.get('lesson_url')
                     if result.get('is_negative'):
                         bot.send_message(
                             chat_id=CBOT_CHAT_ID,
                             text=f'У Вас проверили работу "{result_title}"\n\n'
-                                  'К сожалению в работе нашлись ошибки.\n'
-                                  f'{result_url}'
+                                 'К сожалению в работе нашлись ошибки.\n'
+                                 f'{result_url}'
                         )
                     else:
                         bot.send_message(
                             chat_id=CBOT_CHAT_ID,
                             text=f'У Вас проверили работу "{result_title}"\n\n'
-                                  'Преподавателю все понравилось, '
-                                  f'можно приступать к следующему уроку!\n{result_url}'
+                                 'Преподавателю все понравилось, '
+                                 'можно приступать к следующему уроку!\n'
+                                 f'{result_url}'
                         )
         except ConnectionError as e:
             print(e)
             time.sleep(delay)
         except requests.ReadTimeout:
             pass
-        
+
 
 if __name__ == '__main__':
     main()
